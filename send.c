253747aff9b545d89fec27cbceba380cf7e59a10
Raul Lopez <raul@Rauls-Laptop.local>
2024-10-07 17:10:49 -0700

    Added parameters to command line

--- send.c
+++ send.c
@@ -1,16 +1,36 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
 #define SERVER_PORT 9999    // The port to connect to
 //#define SERVER_IP "137.184.10.4"  // The IP address to connect to (localhost for this example)
-#define SERVER_IP "127.0.0.1"  // The IP address to connect to (localhost for this example)
-#define MESSAGE "Hello from the client!"
+//#define SERVER_IP "127.0.0.1"  // The IP address to connect to (localhost for this example)
+
+#define ARG_COUNT 	3 // IP_ADDRESS and PORT_NUMBER
+#define PROGRAM		0
+#define IP_ADDRESS	1
+#define PORT_NUMBER	2
+#define MESSAGE		3
+
+#define ERROR_ARGS	-1
 
 int main(int argc, char *argv[]) {
+	// check command line parameters
+	if (argc!=(ARG_COUNT+1))
+	{
+		fprintf(stderr,"usage : %s IPV4_address port_number\n",argv[PROGRAM]);
+		return ERROR_ARGS;
+	}
+	// get IP address and port number
+	char ip_address_s[256];
+	strcpy(ip_address_s,argv[IP_ADDRESS]);
+	uint16_t	port_number_ui16;
+	port_number_ui16 = (uint16_t)atoi(argv[PORT_NUMBER]);
+
     // Create socket
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock == -1) {
@@ -26,9 +46,9 @@ int main(int argc, char *argv[]) {
     struct sockaddr_in server_addr;
     memset(&server_addr, 0, sizeof(server_addr));	// reset server address
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(SERVER_PORT);  // Set port, converting to network byte order
+    server_addr.sin_port = htons(port_number_ui16);  // Set port, converting to network byte order
 
-    if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0) {
+    if (inet_pton(AF_INET, ip_address_s, &server_addr.sin_addr) <= 0) {
         perror("Invalid address or address not supported");
         close(sock);
         exit(EXIT_FAILURE);
@@ -49,15 +69,15 @@ int main(int argc, char *argv[]) {
 #endif
 
     // Send data to the server
-	char message[1024];
-	strcpy(message,argv[1]);
-    if (send(sock, message, strlen(message), 0) == -1) {
+	char message_s[1024];
+	strcpy(message_s,argv[MESSAGE]);
+    if (send(sock, message_s, strlen(message_s), 0) == -1) {
         perror("Send failed");
         close(sock);
         exit(EXIT_FAILURE);
     }
 
-    printf("Message sent to server: %s\n", message);
+    printf("Message sent to server: %s\n", message_s);
 
     // Close the socket
     close(sock);
